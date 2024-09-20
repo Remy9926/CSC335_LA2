199a32daa139c9f3c0e53f16c43e351d69df5db4
Ethan <ethanhuang03@gmail.com>
2024-09-20 14:49:16 -0700

    Add ArrayList of books inside LibraryModel

--- src/LibraryModel.java
+++ src/LibraryModel.java
@@ -2,7 +2,17 @@
 // Author(s): Mandy Jiang (user:mandyjiang), Ethan Huang
 // Purpose: Models the library on the backend so that the user doesn't directly interact with it
 
+import java.util.ArrayList;
 
 public class LibraryModel {
 
+	private ArrayList<Book> library;
+	
+	public LibraryModel() {
+		library = new ArrayList<Book>();
+	}
+	
+	void addBookToLibrary(Book book) {
+		library.add(book);
+	}
 }
