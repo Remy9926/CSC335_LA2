ca4d80dba086d0dc37bffc24f8f86bd1e4794d85
Ethan <ethanhuang03@gmail.com>
2024-09-20 13:01:04 -0700

    Add LibraryModel and Parser classes

---
+++ src/LibraryModel.java
@@ -0,0 +1,8 @@
+// File: Book.java
+// Author(s): Mandy Jiang (user:mandyjiang), Ethan Huang
+// Purpose: Models the library on the backend so that the user doesn't directly interact with it
+
+
+public class LibraryModel {
+
+}
---
+++ src/Parser.java
@@ -0,0 +1,21 @@
+// File: Parser.java
+// Author(s): Mandy Jiang (user:mandyjiang), Ethan Huang
+// Purpose: Parses the user input to validate and execute commands as necessary
+
+
+public class Parser {
+	enum Command {
+		SEARCH,
+		ADD_BOOK,
+		SET_TO_READ,
+		RATE,
+		GET_BOOKS,
+		SUGGEST_READ,
+		ADD_BOOKS
+	}
+	
+	public Parser() {
+		
+	}
+
+}
