4f4dae0bf1069a0c51187dab9368586f289c371a
Ethan <ethanhuang03@gmail.com>
2024-09-20 14:36:37 -0700

    Update header of NullCommandException

--- src/NullCommandException.java
+++ src/NullCommandException.java
@@ -1,3 +1,6 @@
+// File: Command.java
+// Author(s): Mandy Jiang (user:mandyjiang), Ethan Huang
+// Purpose: If a user specifies null as the command, this error will be thrown to notify the user
 
 public class NullCommandException extends Exception {
 	public NullCommandException(String input) {
