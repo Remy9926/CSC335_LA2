9a73e577b2f463556151f748504f45ac95d6451b
Ethan <ethanhuang03@gmail.com>
2024-09-20 12:44:43 -0700

    Move MyLibrary and Book to src

---
+++ src/MyLibrary.java
@@ -0,0 +1,9 @@
+// File: Book.java
+// Author(s): Mandy Jiang (user:mandyjiang), Ethan Huang
+// Purpose: Main Text UI of the library that users will interact with
+
+public class MyLibrary {
+	public static void main(String args[]) {
+			System.out.println("Hello, World!");
+	}
+}
