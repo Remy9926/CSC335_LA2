954072a191c1108a3401886f03334a0ad6f08e96
Ethan <ethanhuang03@gmail.com>
2024-09-20 14:26:24 -0700

    Add Command and SearchType as enums

---
+++ src/Command.java
@@ -0,0 +1,16 @@
+// File: Command.java
+// Author(s): Mandy Jiang (user:mandyjiang), Ethan Huang
+// Purpose: Represents the different commands that a user can input
+
+public enum Command {
+	SEARCH,
+	ADD_BOOK,
+	SET_TO_READ,
+	RATE,
+	GET_BOOKS,
+	SUGGEST_READ,
+	ADD_BOOKS,
+	HELP,
+	EXIT,
+	INVALID
+}
---
+++ src/SearchType.java
@@ -0,0 +1,10 @@
+// File: Command.java
+// Author(s): Mandy Jiang (user:mandyjiang), Ethan Huang
+// Purpose: Represents the different search methods that a user can use to search for books
+
+public enum SearchType {
+	TITLE,
+	AUTHOR,
+	RATING,
+	NULL
+}
