28c1c7e170c6b509c141356af0a9eadb949fac07
Ethan <ethanhuang03@gmail.com>
2024-09-22 13:19:07 -0700

    Add descriptions

--- Command.java
+++ Command.java
@@ -2,6 +2,13 @@
 // Author(s): Mandy Jiang (user:mandyjiang), Ethan Huang
 // Purpose: Represents the different commands that a user can input
 
+/*
+ * This enum is representative of the different types of command that the Parser Class can
+ * interpret from the reader. While the Enum does not have any functions itself, the Parser
+ * Class will know how to prompt the user or what messages to display depending on the type
+ * of Command that is currently associated with the Parser, which is achieved everytime the
+ * user gives input into the UI.
+ */
 public enum Command {
 	SEARCH,
 	ADD_BOOK,
--- NullCommandException.java
+++ NullCommandException.java
@@ -2,6 +2,14 @@
 // Author(s): Mandy Jiang (user:mandyjiang), Ethan Huang
 // Purpose: If a user specifies null as the command, this error will be thrown to notify the user
 
+
+/*
+ * This Class is an exception that can be thrown by the Parser Class when the user input read by
+ * the Parser is null. To prevent the Parser from interpreting a null input, there is a guard
+ * clause within the Parser Class that detects if the user input is null, and if so it will
+ * immediately throw this error and let the user know what went wrong.
+ */
+
 public class NullCommandException extends Exception {
 	public NullCommandException(String input) {
 		super("You cannot input null as the command!");
